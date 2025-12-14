a65ab58f521e3fd1bf985966f5b106a9c7a4023f
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-12-14 12:17:31 -0600

    Header

---
+++ src/components/shared/Header/Header.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import NavList from "../NavList/NavList";
+import styles from "./header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link className={styles.logo} to=".">
+          #VANLIFE
+        </Link>
+        <NavList
+          items={[
+            {
+              name: "About",
+              to: "about",
+            },
+            {
+              name: "Vans",
+              to: "vans",
+            },
+          ]}
+        />
+      </nav>
+    </header>
+  );
+}
